7f436e7d96ed32143554476bf99d705a739b4e78
Ana <anapedrabcn@gmail.com>
2025-01-26 21:23:55 +0100

    added new post

---
+++ components/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://tender-sole-27.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- components/providers.tsx
+++ components/providers.tsx
@@ -1,31 +1,34 @@
-'use client'
+"use client";
 
-import { ThemeProvider, useTheme } from 'next-themes'
-import { Toaster } from '@/components/ui/sonner'
+import { ThemeProvider, useTheme } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      enableSystem
-      attribute='class'
-      defaultTheme='dark'
-      disableTransitionOnChange
-    >
-      {children}
-      <ToasterProvider />
-    </ThemeProvider>
-  )
+    <ConvexClientProvider>
+      <ThemeProvider
+        enableSystem
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+      >
+        {children}
+        <ToasterProvider />
+      </ThemeProvider>
+    </ConvexClientProvider>
+  );
 }
 
 function ToasterProvider() {
-  const { resolvedTheme } = useTheme()
+  const { resolvedTheme } = useTheme();
 
   return (
     <Toaster
       richColors
       closeButton
-      position='top-center'
-      theme={resolvedTheme === 'dark' ? 'dark' : 'light'}
+      position="top-center"
+      theme={resolvedTheme === "dark" ? "dark" : "light"}
     />
-  )
+  );
 }
